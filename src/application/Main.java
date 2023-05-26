7b5f87f91b3b7bc09796ba8d66dfe63ddcf86116
SlenderTwo <jonathan.tabares2222@gmail.com>
2023-05-26 18:32:41 +0200

    Login Screen

---
+++ src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//BorderPane root = new BorderPane();
+			//Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			primaryStage.setScene(new Scene(root,520,400));
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
